df5fc1f16557c315cca5dc576a1811fb5a3fc6de
Nicolás Gebauer <negebauerm@gmail.com>
2019-11-08 11:48:32 -0300

    feat: root endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
+# Root application
 class ApplicationController < ActionController::API
+  def root
+    render json: { active: true, datetime: DateTime.new }
+  end
 end
