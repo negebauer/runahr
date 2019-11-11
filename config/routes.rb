3089cdef1ddd4faf2e0751c1ed555f2ea83bcde8
Nicolás Gebauer <negebauerm@gmail.com>
2019-11-11 17:12:23 -0300

    organization add user route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#root'
 
-  resources :organizations, only: %i[index show create]
+  resources :organizations, only: %i[index show create] do
+    member do
+      post 'users' => 'organizations#add_user'
+    end
+  end
+
   get 'users/me' => 'users#me'
   post 'users' => 'users#create'
   post 'login' => 'user_token#create'
