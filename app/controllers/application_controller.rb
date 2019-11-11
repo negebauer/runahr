be87ae6a54cc1b73b7f06ab33127b8b7e61ad999
Nicolás Gebauer <negebauerm@gmail.com>
2019-11-11 17:12:50 -0300

    remove custom current ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,6 @@ class ApplicationController < ActionController::API
 
   private
 
-  def current_ability
-    @current_ability ||= Ability.new(current_user, params)
-  end
-
   def set_current_user_as_user
     @user = current_user
   end
