daba565076c5c45af6549da23611b775f1ba1901
Nicolás Gebauer <negebauerm@gmail.com>
2019-11-11 14:57:16 -0300

    user model helper for organization and role

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,12 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+
+  def organization_user(organization)
+    OrganizationUser.find_by(user_id: id, organization_id: organization.id)
+  end
+
+  def role(organization)
+    organization_user(organization).role
+  end
 end
--- app/views/users/organizations.json.jbuilder
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-json.array! @organizations do |organization|
-  json.partial! 'organizations/organization', organization: organization
-  json.role OrganizationUser.find_by(user_id: @user.id, organization_id: organization.id).role
-end
