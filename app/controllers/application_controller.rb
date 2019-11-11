be0175a30715251cd3e4a6c646d32134c09a9512
Nicolás Gebauer <negebauerm@gmail.com>
2019-11-11 17:12:36 -0300

    current ability with params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::API
 
   private
 
+  def current_ability
+    @current_ability ||= Ability.new(current_user, params)
+  end
+
   def set_current_user_as_user
     @user = current_user
   end
