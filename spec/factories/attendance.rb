8d86d63c52e226a097c0e4950c55b8a892318c79
Nicolás Gebauer <negebauerm@gmail.com>
2019-11-15 18:00:48 -0300

    attendance model spec

---
+++ spec/factories/attendance.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :attendance do
+    user_id { create(:user).id }
+    organization_id { create(:organization).id }
+    check_in_at { DateTime.now }
+  end
+end
