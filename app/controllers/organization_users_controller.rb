016534189f05ceb9d3a16da0687a890d66095680
Nicolás Gebauer <negebauerm@gmail.com>
2019-11-15 03:26:10 -0300

    get organization users

--- app/controllers/organization_users_controller.rb
+++ app/controllers/organization_users_controller.rb
@@ -4,10 +4,14 @@
 class OrganizationUsersController < ApplicationController
   before_action :authenticate_user
 
-  load_and_authorize_resource id_param: 'organization_id'
+  load_and_authorize_resource :organization, id_param: 'organization_id'
+  load_and_authorize_resource :organization_user,
+                              trough: :organization,
+                              only: %i[index show create update destroy]
 
   def index
-    # todo
+    user_ids = @organization_users.pluck(:user_id).uniq
+    @users = User.where(id: user_ids).select(:id, :name).index_by(&:id)
   end
 
   def create
--- app/views/organization_users/_organization_user.json.jbuilder
+++ app/views/organization_users/_organization_user.json.jbuilder
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 
+user ||= nil
+
 json.extract! organization_user, :organization_id, :role, :user_id
+json.user_name user.name if user
