d1e49b629ac9bf9269c3bbb5e9c684bfc01378d7
Nicolás Gebauer <negebauerm@gmail.com>
2019-11-15 16:33:20 -0300

    user spec associations and validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,30 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  name            :string
+#  email           :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:organization_users).dependent(:destroy) }
+    it { should have_many(:organizations).through(:organization_users) }
+    it { should have_many(:attendances).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_uniqueness_of(:email) }
+  end
 end
