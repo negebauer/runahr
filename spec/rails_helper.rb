a04a288df02e07bca83897e984512be355e5852c
Nicolás Gebauer <negebauerm@gmail.com>
2019-11-15 21:13:33 -0300

    json helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'support/auth.rb'
 require 'support/factory_bot.rb'
+require 'support/request_helper.rb'
 require 'support/shared_examples.rb'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -67,6 +68,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Requests::JsonHelpers
 
   Shoulda::Matchers.configure do |shoulda_matchers_config|
     shoulda_matchers_config.integrate do |with|
